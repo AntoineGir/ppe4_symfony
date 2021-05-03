037ce6d572242f74ba78e0e9f50ef932fb63fcf5
AntoineGir <antoine.girard@saintmichelannecy.fr>
2021-05-03 17:18:38 +0200

    add client api

--- src/Entity/Clients.php
+++ src/Entity/Clients.php
@@ -5,9 +5,11 @@ namespace App\Entity;
 use App\Repository\ClientsRepository;
 use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Collection;
+use ApiPlatform\Core\Annotation\ApiResource;
 use Doctrine\ORM\Mapping as ORM;
 
 /**
+ * @ApiResource()
  * @ORM\Entity(repositoryClass=ClientsRepository::class)
  */
 class Clients
